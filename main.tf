59eecbb522feac05d35a3598fb32e7e04867f7a4
Rajesh Sakhamuri <your_email_for_github@example.com>
2026-05-05 09:59:56 +0530

    AI generated terraform infrastructure

--- main.tf
+++ main.tf
@@ -15,41 +15,24 @@ terraform {
 
 
 resource "aws_vpc" "main" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-
-  tags = {
-    Name = "Main VPC"
-  }
+  cidr_block = "10.0.0.0/16"
+  region     = "ap-south-1"
 }
 
 resource "aws_subnet" "public" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = data.aws_availability_zones.available.names[0]
-
-  tags = {
-    Name = "Public Subnet"
-  }
 }
 
 resource "aws_subnet" "private" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = data.aws_availability_zones.available.names[1]
-
-  tags = {
-    Name = "Private Subnet"
-  }
 }
 
-resource "aws_internet_gateway" "main" {
+resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "Main IGW"
-  }
 }
 
 resource "aws_route_table" "public" {
@@ -57,20 +40,12 @@ resource "aws_route_table" "public" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main.id
-  }
-
-  tags = {
-    Name = "Public Route Table"
+    gateway_id = aws_internet_gateway.gw.id
   }
 }
 
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "Private Route Table"
-  }
 }
 
 data "aws_availability_zones" "available" {
